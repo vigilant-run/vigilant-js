d1f48c9c3f3e1f57c60b408ea9b9d4f395150309
Izak Fritz <izakfr@umich.edu>
2025-03-05 08:06:25 -0800

    [#65] Add metric sending

--- react/src/index.ts
+++ react/src/index.ts
@@ -1,3 +1,8 @@
 export { Attributes } from './attributes'
 export { Logger } from './logger'
 export { initErrorHandler, shutdownErrorHandler, captureError } from './errors'
+export {
+  initMetricsHandler,
+  shutdownMetricsHandler,
+  emitMetric,
+} from './metrics'
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ export { Attributes } from './attributes'
 export { Logger, LoggerOptions } from './logger'
 export { addAttributes, clearAttributes, removeAttributes } from './storage'
 export { initErrorHandler, shutdownErrorHandler, captureError } from './errors'
+export {
+  initMetricsHandler,
+  shutdownMetricsHandler,
+  emitMetric,
+} from './metrics'
